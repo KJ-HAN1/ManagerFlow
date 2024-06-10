8d17992d56682831c148c2c8ec240e1d3883777e
KJ-HAN1:git config --global user.name KJ-HAN1git config --global user.name KJ-HAN1 <hkjin@kakao.com>
2024-06-11 02:22:21 +0900

    initial commit

---
+++ managerFlow/src/test/java/com/example/managerFlow/ManagerFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managerFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
