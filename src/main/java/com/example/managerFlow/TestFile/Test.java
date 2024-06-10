138c9846fc42d961ffd692c34d06842fa4c9735c
KJ-HAN1:git config --global user.name KJ-HAN1git config --global user.name KJ-HAN1 <hkjin@kakao.com>
2024-06-11 03:25:26 +0900

    test commit

--- src/main/java/com/example/managerFlow/TestFile/Test.java
+++ src/main/java/com/example/managerFlow/TestFile/Test.java
@@ -1,15 +1,15 @@
 package com.example.managerFlow.TestFile;
 
 public class Test {
-    public static int add(int a, int b){
-        return a + b;
-    }
+//    public static int miii(int a, int b){
+//        return a - b;
+//    }
 
     public static void main(String[] args) {
 
         int a = 3;
         int b = 4;
 
-        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
     }
 }
