9c36cc971ef0bfe60608c735d8b33722e70c3226
cogns8571 <cogns8571@naver.com>
2024-06-11 02:39:46 +0900

    Test file

---
+++ src/main/java/TestFile/Test.java
@@ -0,0 +1,15 @@
+package TestFile;
+
+public class Test {
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 4;
+
+        System.out.println("a + b = " + (a + b));
+    }
+}
